f0e20766df75b23af2324056f8e53dd13efccef4
CuiXiang <xiang.cui@modeling-tech.com>
2024-05-24 15:48:38 +0800

    feat: Icon 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import Calendar from "./Calendar";
-import dayjs, { Dayjs } from "dayjs";
+// import Calendar from "./Calendar";
+// import dayjs, { Dayjs } from "dayjs";
 import "./App.css";
+import { Icon } from "./components/Icon";
+import { createFromIconfont } from "./components/Icon/createFrontIconfont"
+import { IconAdd, IconEmail } from "./components/Icon/icons";
+
+const IconFont = createFromIconfont('//at.alicdn.com/t/c/font_3520073_d8oacpy79dg.js')
 
 function App() {
-  const changeHandler = (date: Dayjs) => {
-    console.log(date.format('YYYY-MM-DD'))
-  };
-
   return (
     <div className="App">
-      <Calendar value={dayjs()} onChange={changeHandler}></Calendar>
+      {/* <Calendar value={dayjs()}></Calendar> */}
+      {/* <Icon></Icon> */}
+      <IconAdd size="40px" color="red"></IconAdd>
+      <IconAdd size="40px" spin></IconAdd>
+      <IconEmail style={{ color: "blue", fontSize: "20px" }} ></IconEmail>
+      <div>iconfont</div>
+      <IconFont type="icon-config" size="30px"></IconFont>
+      <IconFont type="icon-layered-configuration" fill="blue"></IconFont>
     </div>
   );
 }
